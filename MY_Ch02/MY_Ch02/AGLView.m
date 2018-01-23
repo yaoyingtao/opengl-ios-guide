0ca8320e8591e7f2c7877ded60d2c4aee1f801d4
yaoyingtao <yaoyingtao@changba.com>
2018-01-23 10:21:45 +0800

    完成ch2

--- MY_Ch02/MY_Ch02/AGLView.m
+++ MY_Ch02/MY_Ch02/AGLView.m
@@ -9,6 +9,7 @@
 #import "AGLView.h"
 #import <OpenGLES/ES2/gl.h>
 #import <OpenGLES/ES2/glext.h>
+#import <GLKit/GLKit.h>
 
 
 static const float vertices[] =
@@ -19,10 +20,18 @@ static const float vertices[] =
 };
 
 @interface AGLView ()
-@property (nonatomic, strong) EAGLContext *context;
-@property (nonatomic, assign) GLuint frameBuffer;
-@property (nonatomic, assign) GLuint colorBuffer;
-@property (nonatomic, assign) GLuint VAO;
+//@property (nonatomic, strong) EAGLContext *context;
+//@property (nonatomic, assign) GLuint frameBuffer;
+//@property (nonatomic, assign) GLuint colorBuffer;
+//@property (nonatomic, assign) GLuint depthRenderBuffer;
+//@property (nonatomic, assign) GLuint VAO;
+
+@property (nonatomic, strong) CAEAGLLayer *eaglLayer;
+@property (nonatomic, strong) EAGLContext *contex;
+@property (nonatomic, assign) GLuint colorRenderBuffer;
+@property (nonatomic, assign) GLuint depthRenderBuffer;
+@property (nonatomic, assign) GLuint positionSlot;
+
 
 @end
 
@@ -42,44 +51,130 @@ static const float vertices[] =
 }
 
 - (void)commonInit {
-    self.backgroundColor = [UIColor redColor];
-    [self setUpLayer];
-    [self setUpContext];
-    [self setUpFrameBuffer];
-    [self setUpDataBuffer];
-    [self render];
+    [self setupLayer];
+    [self setupContex];
+    [self setupRenderBuffer];
+    [self setupFrameBuffer];
+    [self compileShaders];
+    [self setupVBO];
+//    [self setupDisplayLink];
+    [self render:nil];
 }
 
 #pragma mrak - init
-- (void)setUpLayer {
-    CAEAGLLayer *layer = (CAEAGLLayer*)self.layer;
-    [layer setDrawableProperties:@{kEAGLDrawablePropertyRetainedBacking:@NO, kEAGLDrawablePropertyColorFormat:kEAGLColorFormatRGBA8}];
+- (void)setupLayer {
+    _eaglLayer = (CAEAGLLayer*)self.layer;
+    _eaglLayer.opaque = YES;
 }
 
-- (void)setUpContext {
-    _context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
-    [EAGLContext setCurrentContext:_context];
-}
-
-- (void)setUpFrameBuffer {
-    glGenFramebuffers(1, &_frameBuffer);
-    glBindBuffer(GL_FRAMEBUFFER, _frameBuffer);
+- (void)setupContex {
+    _contex = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    if (!_contex) {
+        NSLog(@"failed to create contex opengles 3");
+        exit(1);
+    }
     
-    glGenRenderbuffers(1, &_colorBuffer);
-    glBindRenderbuffer(GL_RENDERBUFFER, _colorBuffer);
+    if (![EAGLContext setCurrentContext:_contex]) {
+        NSLog(@"failed to set current opengl context");
+        exit(1);
+    }
     
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _colorBuffer);
 }
 
-- (void)setUpDataBuffer {
-    glGenBuffers(1, &_VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, _VAO);
+- (void)setupRenderBuffer {
+    glGenRenderbuffers(1, &_colorRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+    [_contex renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
+}
+
+- (void)setupVBO {
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
-- (void)render {
+- (void)setupFrameBuffer {
+    GLuint framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                              GL_RENDERBUFFER, _colorRenderBuffer);
+}
+
+- (void)render:(CADisplayLink*)displayLink {
+    glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+    [_contex presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+- (void)compileShaders {
+    GLuint vertexShader = [self compileShader:@"SimpleVertex" withType:GL_VERTEX_SHADER];
+    GLuint fragementShader = [self compileShader:@"SimpleFragment" withType:GL_FRAGMENT_SHADER];
+    
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragementShader);
+    glLinkProgram(programHandle);
+    
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    glUseProgram(programHandle);
+    
+    _positionSlot = glGetAttribLocation(programHandle, "Position");
+    
+    glEnableVertexAttribArray(_positionSlot);
     
 }
 
+- (void)setupDisplayLink {
+    CADisplayLink *displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render:)];
+    [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+}
+
+
+#pragma mark - tool
+- (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType {
+    NSString *shaderPath = [[NSBundle mainBundle] pathForResource:shaderName ofType:@"glsl"];
+    NSError *error;
+    NSString *shaderString = [NSString stringWithContentsOfFile:shaderPath encoding:NSUTF8StringEncoding error:&error];
+    if (!shaderString) {
+        NSLog(@"error loading shader");
+        exit(1);
+    }
+    
+    GLuint shaderHandle = glCreateShader(shaderType);
+    
+    const char *shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = [shaderString length];
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    glCompileShader(shaderHandle);
+    
+    GLuint complieSuccess;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &complieSuccess);
+    if (complieSuccess == GL_FALSE) {
+        GLchar message[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(message), 0, &message[0]);
+        NSString *messageString = [NSString stringWithUTF8String:message];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    return shaderHandle;
+}
 
 @end

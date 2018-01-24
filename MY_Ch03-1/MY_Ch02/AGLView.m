1bc1ba5d0a94e43e87d8bdc5460f535f06444718
Yaoyt <yaoyt@163.com>
2018-01-24 23:12:05 +0800

    完成绘制3-1

---
+++ MY_Ch03-1/MY_Ch02/AGLView.m
@@ -0,0 +1,239 @@
+//
+//  AGLView.m
+//  MY_Ch02
+//
+//  Created by tomy yao on 2018/1/22.
+//  Copyright © 2018年 tomy yao. All rights reserved.
+//
+
+#import "AGLView.h"
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+#import <GLKit/GLKit.h>
+
+
+typedef struct {
+    float Position[3];
+    float Color[2];
+}SceneVertex;
+
+/////////////////////////////////////////////////////////////////
+// Define vertex data for a triangle to use in example
+static const SceneVertex vertices[] =
+{
+    {{-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f}}, // lower left corner
+    {{ 0.5f, -0.5f, 0.0f}, {1.0f, 0.0f}}, // lower right corner
+    {{-0.5f,  0.5f, 0.0f}, {0.0f, 1.0f}}, // upper left corner
+    {{0.5f,  0.5f, 0.0f}, {1.0f, 1.0f}}, // upper left corner
+};
+
+const GLubyte indices[] = {
+    // Front
+    0, 1, 2,
+    2, 3, 1,
+};
+
+@interface AGLView ()
+@property (nonatomic, strong) CAEAGLLayer *eaglLayer;
+@property (nonatomic, strong) EAGLContext *contex;
+@property (nonatomic, assign) GLuint indexBuffer;
+@property (nonatomic, assign) GLuint colorRenderBuffer;
+@property (nonatomic, assign) GLuint depthRenderBuffer;
+@property (nonatomic, assign) GLuint positionSlot;
+@property (nonatomic, assign) GLuint texCoordSlot;
+@property (nonatomic, assign) GLuint textureUniform;
+
+@property (nonatomic, assign) GLuint leafTexture;
+
+
+@end
+
+@implementation AGLView
+
+#pragma mark - life cycle
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
++ (Class)layerClass {
+    return [CAEAGLLayer class];
+}
+
+- (void)commonInit {
+    [self setupLayer];
+    [self setupContex];
+    [self setupRenderBuffer];
+    [self setupFrameBuffer];
+    [self compileShaders];
+    [self setupVBO];
+    _leafTexture = [self setupTexture:@"tile_floor.png"];
+//    [self setupDisplayLink];
+    [self render:nil];
+}
+
+#pragma mrak - init
+- (void)setupLayer {
+    _eaglLayer = (CAEAGLLayer*)self.layer;
+    _eaglLayer.opaque = YES;
+}
+
+- (void)setupContex {
+    _contex = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    if (!_contex) {
+        NSLog(@"failed to create contex opengles 3");
+        exit(1);
+    }
+    
+    if (![EAGLContext setCurrentContext:_contex]) {
+        NSLog(@"failed to set current opengl context");
+        exit(1);
+    }
+    
+}
+
+- (void)setupRenderBuffer {
+    glGenRenderbuffers(1, &_colorRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+    [_contex renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
+}
+
+- (void)setupVBO {
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+- (void)setupFrameBuffer {
+    GLuint framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                              GL_RENDERBUFFER, _colorRenderBuffer);
+}
+
+- (void)render:(CADisplayLink*)displayLink {
+    glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), 0);
+    glVertexAttribPointer(_texCoordSlot, 2, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), (GLvoid*)(3*sizeof(float)));
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, _leafTexture);
+    glUniform1f(_textureUniform, 0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    
+    glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(indices[0]), GL_UNSIGNED_BYTE, 0);
+//    glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices)/sizeof(SceneVertex));
+    
+    [_contex presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+- (void)compileShaders {
+    GLuint vertexShader = [self compileShader:@"SimpleVertex" withType:GL_VERTEX_SHADER];
+    GLuint fragementShader = [self compileShader:@"SimpleFragment" withType:GL_FRAGMENT_SHADER];
+    
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragementShader);
+    glLinkProgram(programHandle);
+    
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    glUseProgram(programHandle);
+    
+    _positionSlot = glGetAttribLocation(programHandle, "Position");
+    _texCoordSlot = glGetAttribLocation(programHandle, "TexCoordIn");
+
+    _textureUniform = glGetUniformLocation(programHandle, "Texture");
+
+    
+    glEnableVertexAttribArray(_positionSlot);
+    glEnableVertexAttribArray(_texCoordSlot);
+
+}
+
+- (void)setupDisplayLink {
+    CADisplayLink *displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render:)];
+    [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+}
+
+
+#pragma mark - tool
+- (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType {
+    NSString *shaderPath = [[NSBundle mainBundle] pathForResource:shaderName ofType:@"glsl"];
+    NSError *error;
+    NSString *shaderString = [NSString stringWithContentsOfFile:shaderPath encoding:NSUTF8StringEncoding error:&error];
+    if (!shaderString) {
+        NSLog(@"error loading shader");
+        exit(1);
+    }
+    
+    GLuint shaderHandle = glCreateShader(shaderType);
+    
+    const char *shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = [shaderString length];
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    glCompileShader(shaderHandle);
+    
+    GLuint complieSuccess;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &complieSuccess);
+    if (complieSuccess == GL_FALSE) {
+        GLchar message[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(message), 0, &message[0]);
+        NSString *messageString = [NSString stringWithUTF8String:message];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    return shaderHandle;
+}
+
+- (GLuint)setupTexture:(NSString*)fileName {
+    CGImageRef spirteImage = [UIImage imageNamed:fileName].CGImage;
+    if (!spirteImage) {
+        NSLog(@"fiale to load image");
+        exit(1);
+    }
+    size_t width = CGImageGetWidth(spirteImage);
+    size_t height = CGImageGetHeight(spirteImage);
+    
+    GLubyte *spirteData = (GLubyte*)calloc(width * height * 4, sizeof(GLubyte));
+    CGContextRef spriteContext = CGBitmapContextCreate(spirteData, width, height, 8, width * 4, CGImageGetColorSpace(spirteImage), kCGImageAlphaPremultipliedLast);
+    
+    CGContextDrawImage(spriteContext, CGRectMake(0, 0, width, height), spirteImage);
+    CGContextRelease(spriteContext);
+    
+    GLuint texName;
+    glGenTextures(1, &texName);
+    glBindTexture(GL_TEXTURE_2D, texName);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, spirteData);
+    
+    free(spirteData);
+    return texName;
+}
+
+@end
